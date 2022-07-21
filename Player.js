a7ba2af502069c7cedf6bc99a93cfa7c3a4cd79f
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-07-21 15:44:55 +0000

    The movement is very functional (:

--- Player.js
+++ Player.js
@@ -1,15 +1,62 @@
 import { player, background } from "./script.js";
+var tracktick = "begin"; // this variable keeps the 
+                      // key inputs in sync
+  var past_x;
+  var past_y;
 
 // this is called from script.js in the run function
 export function tick() {
+  if(tracktick = "begin") {
+    past_x = player.x;
+    past_y = player.y;
+  }
   Player.style.left = player.x + "px";
   Player.style.bottom = player.y + "px";
-  console.log("x= "+ background.x + "y= " + background.y);
+  console.log("x= "+ player.x + " y= " + player.y);
+  tracktick = true;
 }
 //Player is only for the css, player is the numbers
+
+
 function move(xc, yc) {
-    background.x -= xc;
-    background.y -= yc;  
+console.log("keylistener run",tracktick);
+  if(tracktick) {
+    console.log("tick allow",tracktick);
+
+
+  if (player.x > 50&&
+      player.x < 600) {
+        console.log(past_x);
+        player.x += xc;   
+      }
+
+  if (player.y > 50&&
+      player.y < 400) {
+      player.y += yc;   
+      }
+    
+  if (player.y > 50&&
+      player.y < 400) {
+        past_y = player.y;
+      } else {
+        background.y -= yc;
+        player.y = past_y;
+      }
+    
+  if (player.x > 50&&
+      player.x < 600) {
+        past_x = player.x;
+      } else {
+        background.x -= xc;
+        player.x = past_x;
+
+      }
+    console.log(player.x);
+    
+  past_x = player.x;
+  past_y = player.y;
+    tracktick = false;
+    }
   }
 
 document.addEventListener("keydown", event => {
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import {tick as Playertick} from "./Player.js";
 import {tick as Maptick} from "./Map.js";
+
 class moveable {
   constructor(x = 0, y = 0) {
     this.x = x;
@@ -12,7 +13,8 @@ export const background = new moveable(-2500, -2500);
 function run() {
   Playertick();
   Maptick();
-    setTimeout(run, 0);
+    setTimeout(run, 50); // this controls how fast
+                         // the program can run
 }
 
 run();
